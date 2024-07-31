bd9f4d56f9241679b1a5604706afb8a7ced76ec4
Sridhar Raj <94793363+SridharRaj980@users.noreply.github.com>
2024-07-31 17:53:38 +0530

    Update WelcomePage.js

--- src/Components/Pages/WelcomePage.js
+++ src/Components/Pages/WelcomePage.js
@@ -1,38 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid, Typography, Button } from '@mui/material';
 import { ThemeProvider } from '@mui/material';
 import theme from '../../Theme';
 import { auth, googleProvider } from '../../firebase';
-import { signInWithRedirect, getRedirectResult } from 'firebase/auth';
+import { signInWithPopup, onAuthStateChanged, signOut } from 'firebase/auth';
 import { FaGoogle } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 const WelcomePage = () => {
   const navigate = useNavigate();
+  const [user, setUser] = useState(null);
 
   const handleSignIn = async () => {
     try {
-      await signInWithRedirect(auth, googleProvider);
+      const result = await signInWithPopup(auth, googleProvider);
+      setUser(result.user);
+      console.log('User Info:', result.user); // User info
+      navigate('/home'); // Redirect to your dashboard or main page
     } catch (error) {
       console.error('Error during sign-in: ', error);
     }
   };
 
-  const handleRedirect = async () => {
+  const handleLogout = async () => {
     try {
-      const result = await getRedirectResult(auth);
-      if (result && result.user) {
-        console.log('User Info:', result.user); // User info
-        navigate('/dashboard'); // Redirect to your dashboard or main page
-      }
+      await signOut(auth);
+      setUser(null);
     } catch (error) {
-      console.error('Error during redirect result: ', error);
+      console.error('Error during sign-out: ', error);
     }
   };
 
   useEffect(() => {
-    handleRedirect();
-  }, []);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        navigate('/home'); // Redirect to your dashboard or main page
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [navigate]);
 
   const iconStyle = {
     marginRight: '8px',
@@ -45,23 +55,26 @@ const WelcomePage = () => {
       <style>{`body { background-color: #F4F5FA; }`}</style>
       <Grid container justifyContent="center">
         <Grid item xs={10} sm={4} container justifyContent="center" direction="column">
-          <Typography variant="h4" style={{ marginTop: '76px' }}>
-            Welcome to Task Manager
-          </Typography>
-          <Typography variant="body1" style={{ color: theme.palette.quaternary.main, marginTop: '100px' }}>
-            Login into your account
-          </Typography>
-          <Typography variant="body2" style={{ color: theme.palette.tertiary.main, marginTop: '20px' }}>
-            Let us make the circle bigger!
-          </Typography>
-          <Button
-            onClick={handleSignIn}
-            variant="contained"
-            color="primary"
-            sx={{ marginTop: '100px' }}
-          >
-            <FaGoogle style={iconStyle} /> Sign in with Google
-          </Button>
+          
+              <Typography variant="h4" style={{ marginTop: '76px' }}>
+                Welcome to Task Manager
+              </Typography>
+              <Typography variant="body1" style={{ color: theme.palette.quaternary.main, marginTop: '100px' }}>
+                Login into your account
+              </Typography>
+              <Typography variant="body2" style={{ color: theme.palette.tertiary.main, marginTop: '20px' }}>
+                Let us make the circle bigger!
+              </Typography>
+              <Button
+                onClick={handleSignIn}
+                variant="contained"
+                color="primary"
+                sx={{ marginTop: '100px' }}
+              >
+                <FaGoogle style={iconStyle} /> Sign in with Google
+              </Button>
+           
+          
         </Grid>
       </Grid>
     </ThemeProvider>
